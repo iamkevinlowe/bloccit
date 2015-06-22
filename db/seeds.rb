de1ef2354c07c1d0188aee3115c1b8bec4fd3364
Kevin Lowe <iamkevinlowe@gmail.com>
2015-06-21 20:36:49 -0700

    Checkpoint 38 completed, Associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,21 @@
 require 'faker'
 
+# Create Users
+5.times do
+  user = User.new(
+    name:       Faker::Name.name,
+    email:      Faker::Internet.email,
+    password:   Faker::Lorem.characters(10)
+    )
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all
+
 # Create Posts
 50.times do 
   Post.create!(
+    user:   users.sample,
     title:  Faker::Lorem.sentence,
     body:   Faker::Lorem.paragraph
     )
@@ -11,10 +24,18 @@ posts = Post.all
 
 100.times do
   Comment.create!(
+    # user: users.sample,   # we have not yet associated Users with Comments
     post:   posts.sample,
     body:   Faker::Lorem.paragraph
     )
 end
+
+user = User.first
+user.skip_reconfirmation!
+user.update_attributes!(
+  email: 'hello@bloccit.com',
+  password: 'world123'
+)
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
