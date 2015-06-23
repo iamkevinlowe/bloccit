cb20867ec3ea4b53467a7071ec8bf7d197a443ae
Kevin Lowe <iamkevinlowe@gmail.com>
2015-06-22 19:59:00 -0700

    Checkpoint 40 completed, Topics and Posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,12 +12,22 @@ require 'faker'
 end
 users = User.all
 
+# Create Topics
+15.times do
+  Topic.create!(
+    name:         Faker::Lorem.sentence,
+    description:  Faker::Lorem.paragraph
+    )
+end
+topics = Topic.all
+
 # Create Posts
 50.times do 
   Post.create!(
     user:   users.sample,
     title:  Faker::Lorem.sentence,
-    body:   Faker::Lorem.paragraph
+    body:   Faker::Lorem.paragraph,
+    topic:   topics.sample
     )
 end
 posts = Post.all
@@ -30,13 +40,37 @@ posts = Post.all
     )
 end
 
-user = User.first
-user.skip_reconfirmation!
-user.update_attributes!(
-  email: 'hello@bloccit.com',
-  password: 'world123'
-)
+# Create an admin user
+admin = User.new(
+  name:       'Admin User',
+  email:      'admin@example.com',
+  password:   'helloworld',
+  role:       'admin'
+  )
+admin.skip_confirmation!
+admin.save!
+
+# Create a moderator
+moderator = User.new(
+  name:       'Moderator User',
+  email:      'moderator@example.com',
+  password:   'helloworld',
+  role:       'moderator'
+  )
+moderator.skip_confirmation!
+moderator.save!
+
+# Create a member
+member = User.new(
+  name:       'Member User',
+  email:      'member@example.com',
+  password:   'helloworld'
+  )
+member.skip_confirmation!
+member.save!
 
 puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
