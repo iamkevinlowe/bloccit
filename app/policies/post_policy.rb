3a3d9cb964841c076f4f868b3aa5f9fe53429274
Kevin Lowe <iamkevinlowe@gmail.com>
2015-06-23 15:40:54 -0700

    Used scope.none on post_policy

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,4 +1,24 @@
 class PostPolicy < ApplicationPolicy
+  class Scope < Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      if user.nil?
+        scope.none
+      elsif user.admin? || user.moderator?
+        scope.all
+      elsif user.present?
+        scope.where(:user_id => user.id)
+      end
+    end
+
+  end
+
   def index?
     true
   end
